d2437da228fc731d01636f4f12cc511bfedb780e
Daniel Maslowski <daniel.maslowski@shop.co>
2016-08-06 01:02:38 +0200

    +animals and facial parts

--- src/index.js
+++ src/index.js
@@ -13,3 +13,5 @@ export const kittyEmoji = emoji(0x1F638, 0x1F640);
 export const catfaceEmoji = kittyEmoji;
 export const turtleEmoji = emoji(128034, 128034);
 export const noEvilEmoji = emoji(128584, 128586);
+export const animalEmoji = emoji(128000, 128063);
+export const facialPartsEmoji = emoji(128064, 128069);
